511355208051dda610e1b997781a39b75ac6893a
Nick Ronnei <nronnei@gmail.com>
2016-09-19 00:52:27 -0400

    fully implements the idea via live feeds.

--- example.js
+++ example.js
@@ -1,62 +1,92 @@
-const data = require("./index.js");
-const fs = require("fs");
-const request = require("request")
+const g2g = require("./index.js");
+const request = require("request");
+const moment = require("moment");
 
-// let test_obj = {};
-// data.buildFeatureCollection((fc) => {
-//   test_obj = fc;
-//   for (station of test_obj.features) {
-//     let lastReport = station.last_updated
-//     console.log(lastReport);
-//   }
-// });
+// First, let's decalre the variable that will hold our featueCollection
+let myFC = undefined;
 
-
+// Next, let's use time to show how the module works
+// Start by setting our current time for comparison
+const startTime = moment();
+// Log that time so we can compare it to our other output
+console.log("\nCurrent time: " + startTime.format("dddd, MMM Do - h:mm a"));
 
 
-// data.getSystemJson(
-//   "https://api-core.niceridemn.org/gbfs/en/system_information.json",
-//   (info) => {
-//   console.log(info);
-// });
+// Now lets build an actual FeatureCollection!
+// This method would be used to instantiate your GeoJSON Layer
+// Call buildFeatureCollection only for setting up an entirely new layer
+g2g.buildFeatureCollection((fc) => {
+
+  // To demonstrate that the callback is working, set myFC equal to the result
+  // of buildFeatureCollection
+  myFC = fc;
+
+  // Notice that myFC is now a fully functional GeoJSON Feature Collection
+  // Iterate over its features
+  for (feature of myFC.features.slice(100, 105)) {
+    console.log("\n");
+
+    // Set time vars for convenience
+    let updateTime = moment.unix(feature.last_updated);
+    let reportTime = moment.unix(parseInt(feature.properties.station_status.last_reported));
+
+    // Log last updated
+    console.log(
+      "Station '"
+      + feature.station_id +
+      "' last updated at "
+      + updateTime.format("dddd, MMM Do - h:mm a")
+    );
+
+    // Log last reported
+    console.log(
+      "Station '"
+      + feature.station_id +
+      "' last reported at "
+      + reportTime.format("dddd, MMM Do - h:mm a")
+    );
+
+  }
+
+  // Formatting, you can ignore this
+  console.log("\n\n");
+  console.log("There are currently " + myFC.features.length + " features in myFC");
+  console.log("waiting for update...");
+  console.log("\n");
+
+});
 
 
+// Now lets simulate an update!
+// We'll wait 60 seconds, then grab an update.
+// Then, we'll print all the same stuff from before
+const delay = 60000;
+setTimeout(() => {
+  g2g.updateStationStatus(myFC, (updatedFeatureClass) => {
+    for (feature of updatedFeatureClass.features.slice(100, 105)) {
+      console.log("\n");
 
+      // Set time vars for convenience
+      let updateTime = moment.unix(feature.last_updated);
+      let reportTime = moment.unix(parseInt(feature.properties.station_status.last_reported));
 
-let cb = () => {
-  request("https://api-core.niceridemn.org/gbfs/en/station_status.json", (err, res, body) => {
-    if (err) {
-      return console.lerror(err);
+      // Log last updated
+      console.log(
+        "Station '"
+        + feature.station_id +
+        "' last updated at "
+        + updateTime.format("dddd, MMM Do - h:mm a")
+      );
+
+      // Log last reported
+      console.log(
+        "Station '"
+        + feature.station_id +
+        "' last reported at "
+        + reportTime.format("dddd, MMM Do - h:mm a")
+      );
+
     }
-    let data = JSON.parse(body)
-    console.log(data.data.stations[10].last_reported);
-  })
-}
-cb();
-setTimeout(cb, 61000)
-
-
-// fs.readFile("./data/station_status.json", "utf-8", (err, stationJson) => {
-//   if (err) {
-//     return console.err(err);
-//   }
-//   // let stations = stationJson.data.stations;
-//   // cb(stations)
-//   // console.log(Object.keys(stationJson));
-//   console.log(stationJson.data.stations);
-// });
-
-
-// request("https://api-core.niceridemn.org/gbfs/en/station_status.json", (err, res, body) => {
-//   if (err) {
-//     return console.lerror(err);
-//   }
-//   let data = JSON.parse(body)
-//   console.log(data.last_updated);
-// })
-
-
-//
-// const ops = {
-//
-// }
+    console.log("\n");
+  });
+}, delay);
--- index.js
+++ index.js
@@ -1,181 +1,5 @@
 const request = require("request");
-const fs = require("fs");
 
-
-// module.exports = {
-//
-//   FeatureCollection: (system_info) => {
-//     if (system_info === undefined) {
-//       return {
-//         "type": "FeatureCollection",
-//         "system_info": undefined,
-//         "features": []
-//       };
-//     } else {
-//       if (typeof(system_info) !== 'object') {
-//         throw new TypeError("Failed to create Feature, invalid JSON input.");
-//       }
-//       if(system_info.hasOwnProperty('length')) {
-//         throw new TypeError("Failed to create Feature, invalid JSON input.");
-//       }
-//       return {
-//         "type": "FeatureCollection",
-//         "system_info": system_info,
-//         "features": []
-//       };
-//
-//     }
-//
-//   },
-//
-//   Feature: (station_info) => {
-//
-//     if (typeof(station_info) !== 'object') {
-//       throw new TypeError("Failed to create Feature, invalid JSON input.");
-//     }
-//     if(station_info.hasOwnProperty('length')) {
-//       throw new TypeError("Failed to create Feature, invalid JSON input.");
-//     }
-//     return {
-//       "type": "Feature",
-//       "station_id": station_info.station_id,
-//       "geometry": {
-//         "type": "Point",
-//         "coordinates": [station_info.lon, station_info.lat]
-//       },
-//       "properties": {
-//         "station_info": station_info,
-//         "station_status": undefined
-//       }
-//     };
-//
-//   },
-//
-//   getCachedStationJson: (path, callback) => {
-//     fs.readFile(path, "utf-8", (err, stationJson) => {
-//       if (err) {
-//         return console.err(err);
-//       }
-//       let stations = stationJson.data.stations;
-//       callback(stations)
-//     });
-//   },
-//
-//   getCachedSystemJson: (path, callback) => {
-//     fs.readFile(path, "utf-8", (err, systemJson) => {
-//       if (err) {
-//         return console.err(err);
-//       }
-//       let systemInfo = systemJson.data;
-//       callback(systemInfo);
-//     });
-//   },
-//
-//   // updateStationStatus: (featureCollection) => {
-//   //   exports.getCachedStationJson(feeds[1], (statusUpdate) => {
-//   //     for (update of statusUpdate) {
-//   //       let id = update.station_id;
-//   //       for (station of featureCollection.features) {
-//   //         if (id = station.station_id) {
-//   //           station.properties.station_status = update;
-//   //         }
-//   //       }
-//   //     }
-//   //   })
-//   //
-//   // },
-//
-//
-//
-//
-// // LIVE
-//
-//   getStationJson: function(url, callback) {
-//     request(url, (err, res, body) => {
-//       if (!error && res.statusCode == 200) {
-//         callback(body.data.stations)
-//       } else if (err) {
-//         return console.err(err);
-//       }
-//     });
-//   },
-//
-//   getSystemJson: function(url, callback) {
-//     request(url, (err, res, body) => {
-//       if (!error && res.statusCode == 200) {
-//         callback(body.data)
-//       } else if (err) {
-//         return console.err(err);
-//       }
-//     });
-//   },
-//
-//   updateStationStatus: (featureCollection, callback) => {
-//     // Get Station Status
-//     getStationJson("https://api-core.niceridemn.org/gbfs/en/station_status.json", (stationUpdates) => {
-//       // For each Station Status object
-//       for (update of stationUpdates) {
-//
-//         // Get the station_id
-//         let id = update.station_id;
-//
-//         // Match the station_id of the update to one in featureCollection
-//         for (station of featureCollection.features) {
-//           if (id = station.station_id) {
-//             station.properties.station_status = update;
-//           }
-//         }
-//         // A Thought: this would be more efficient if it was indexed.
-//
-//       }
-//       // Make the result accessible to the callback after updating all features
-//       callback(featureCollection)
-//     });
-//
-//   },
-//
-//   buildFeatureCollection: (callback) => {
-//     let featureCollection = undefined;
-//
-//     // Get System Information
-//     exports.getSystemJson("https://api-core.niceridemn.org/gbfs/en/system_information.json", (systemData) => {
-//
-//       // Build featureCollection using System Information
-//       featureCollection = FeatureCollection(systemData);
-//
-//       // Get Station Information
-//       exports.getStationJson("https://api-core.niceridemn.org/gbfs/en/station_information.json", (stations) => {
-//         // Populate featureCollection.features
-//         for (station of stations) {
-//           let feature = exports.Feature(station);
-//           featureCollection.features.push(feature);
-//         }
-//         // Get Station Status, update featureCollection
-//         exports.updateStationStatus(featureCollection, callback);
-//
-//       });
-//     });
-//   }
-//
-//
-//   // buildFeatureCollection: () => {
-//   //   // Generate base FeatureCollection
-//   //   const fc = undefined;
-//   //   exports.getCachedSystemJson(feeds[2], (data) => {
-//   //     fc = exports.FeatureCollection(data);
-//   //   });
-//   //   // Get Station Information
-//   //   exports.getCachedStationJson(feeds[0], (stations) => {
-//   //     for (station of stations) {
-//   //       // Use System Information to create Features and push them to the FC
-//   //       fc.features.push(Feature(station));
-//   //     }
-//   //   });
-//   //   // Get Station Status
-//   //   exports.updateStationStatus(fc);
-//   // }
-//
-// };
 
 
 const FeatureCollection = (system_info) => {
@@ -226,32 +50,29 @@ const Feature = (station_info) => {
 
 };
 
-const getStationJson = (url, callback) => {
-  request(url, (err, res, body) => {
+const getFeed = (feedUrl, callback) => {
+  request(feedUrl, (err, res, body) => {
     if (!err && res.statusCode == 200) {
-      let data = JSON.parse(body);
-      callback(data.data.stations)
-    } else if (err) {
-      return console.error(err);
-    }
-  });
-};
 
-const getSystemJson = (url, callback) => {
-  request(url, (err, res, body) => {
-    if (!err && res.statusCode == 200) {
       let data = JSON.parse(body);
-      callback(data.data)
+      callback(data)
+
     } else if (err) {
+
       return console.error(err);
+
     }
   });
 };
 
 const updateStationStatus = (featureCollection, callback) => {
   // Get Station Status
-  getStationJson("https://api-core.niceridemn.org/gbfs/en/station_status.json", (stationUpdates) => {
-    // For each Station Status object
+  getFeed("https://api-core.niceridemn.org/gbfs/en/station_status.json", (statusUpdate) => {
+
+    let updateTime = statusUpdate.last_updated;
+    let stationUpdates = statusUpdate.data.stations;
+
+    // For each station update
     for (update of stationUpdates) {
 
       // Get the station_id
@@ -259,14 +80,15 @@ const updateStationStatus = (featureCollection, callback) => {
 
       // Match the station_id of the update to one in featureCollection
       for (station of featureCollection.features) {
-        if (id = station.station_id) {
+        if (id == station.station_id) {
+          station.last_updated = updateTime;
           station.properties.station_status = update;
         }
       }
       // A Thought: this would be more efficient if it was indexed.
 
     }
-    // Make the result accessible to the callback after updating all features
+    // Make the updated featureCollection accessible to the callback
     callback(featureCollection)
   });
 
@@ -276,13 +98,14 @@ const buildFeatureCollection = (callback) => {
   let featureCollection = undefined;
 
   // Get System Information
-  getSystemJson("https://api-core.niceridemn.org/gbfs/en/system_information.json", (systemData) => {
+  getFeed("https://api-core.niceridemn.org/gbfs/en/system_information.json", (systemData) => {
 
     // Build featureCollection using System Information
-    let featureCollection = FeatureCollection(systemData);
+    let featureCollection = FeatureCollection(systemData.data);
 
     // Get Station Information
-    getStationJson("https://api-core.niceridemn.org/gbfs/en/station_information.json", (stations) => {
+    getFeed("https://api-core.niceridemn.org/gbfs/en/station_information.json", (stationInfo) => {
+      let stations = stationInfo.data.stations;
       // Populate featureCollection.features
       for (station of stations) {
         let feature = Feature(station);
@@ -297,10 +120,13 @@ const buildFeatureCollection = (callback) => {
   });
 };
 
+
+
 // Export all the functions
-exports.FeatureCollection = FeatureCollection;
-exports.Feature = Feature;
-exports.getStationJson = getStationJson;
-exports.getSystemJson = getSystemJson;
-exports.updateStationStatus = updateStationStatus;
-exports.buildFeatureCollection = buildFeatureCollection;
+module.exports = {
+  FeatureCollection: FeatureCollection,
+  Feature: Feature,
+  getFeed: getFeed,
+  updateStationStatus: updateStationStatus,
+  buildFeatureCollection: buildFeatureCollection
+}
